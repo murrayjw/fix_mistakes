5998fa64d5cc6f9b66c312bae02e90d62f2f74e3
murrayjw <42420890+murrayjw@users.noreply.github.com>
2020-11-03 07:43:13 -0500

    added utils file

---
+++ utils.R
@@ -0,0 +1,24 @@
+#' Paste a comma separated vector of strings
+#'
+#' 
+#' @param x A vector of strings
+#'
+#' @return the strings in a comma separated vector
+#' 
+#' @example 
+#' 
+#' paste_comma(unique(iris$Species))
+#' 
+#' @export
+#'
+#'
+paste_comma <- function(x) {
+  cat(paste(shQuote(x, type="cmd"), collapse=", "))
+}
+
+
+fahrenheit_to_celsius <- function(temp_f) {
+  temp_c <- (temp_f - 32) * 5 / 9
+  return(temp_c)
+}
+
